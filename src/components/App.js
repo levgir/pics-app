1be7c251a07fe68ced8ef3b9badefc9850287b2c
Levi Giraud <levigiraud@Levis-MacBook-Air.local>
2020-03-22 21:04:19 -0600

    installed axios and connected it to unsplash api, have a basic search function now console logging results

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" styel={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID xRkexxYLwZPFebbQek5KY3s4Eq5RGY72Zq0HhVPALik"
+      }
+    });
+    console.log(response.data.results);
+  }
+
+  render() {
+    return (
+      <div className="ui container" styel={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
